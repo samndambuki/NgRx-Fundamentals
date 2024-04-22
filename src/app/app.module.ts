a30abf8097b45759b64f3ddd361b3f5f6b01957f
samndambuki <sam.ndambuki08@gmail.com>
2024-04-22 15:54:06 +0300

    dispatched an action to chnage state

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { StoreModule } from '@ngrx/store';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InMemoryDataService } from './in-memory-data.service';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService),
+    StoreModule.forRoot({}),
+    StoreModule.forRoot({}, {}),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
---
+++ src/app/products/state/products.reducer.ts
@@ -0,0 +1,17 @@
+import { createAction, createReducer, on } from "@ngrx/store";
+
+export interface ProductsState {
+    showProductCode: boolean;
+}
+
+const initialState: ProductsState = {
+    showProductCode: true
+}
+
+export const productsReducer = createReducer(
+    initialState,
+    on(createAction('[Products Page] Toggle Show Product Code'), (state) => ({
+        ...state,
+        showProductCode: !state.showProductCode
+    }))
+)
